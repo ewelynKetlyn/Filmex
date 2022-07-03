7f8e5bb06d7ada0cb3713d11fc6bfd4d8dd2ab04
Ewelyn <ewelyn.silva@picpay.com>
2022-07-03 10:33:58 -0300

    Commit com criação do adapter

--- app/src/main/java/com/example/filmex/MainActivity.kt
+++ app/src/main/java/com/example/filmex/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.filmex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.filmex.adapter.MainAdapter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rv: RecyclerView = findViewById(R.id.recycler_view)
+        val adapter = MainAdapter()
+        rv.layoutManager = LinearLayoutManager(this)
+        rv.adapter = adapter
     }
+
+
 }
