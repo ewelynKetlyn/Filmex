a6b1994e84eb7a8139cc16d05953c7d3f7ef2495
Ewelyn <ewelyn.silva@picpay.com>
2022-07-04 17:06:54 -0300

    Commit com implementação das recyclerView verticais

--- app/src/main/java/com/example/filmex/MainActivity.kt
+++ app/src/main/java/com/example/filmex/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.filmex.adapter.MainAdapter
+import com.example.filmex.model.Category
 import com.example.filmex.model.Movie
 
 class MainActivity : AppCompatActivity() {
@@ -12,15 +12,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val movies = mutableListOf<Movie>()
-        for(i in 0 until 6){
-            val movie = Movie(R.drawable.img_1)
-            movies.add(movie)
+        val categories = mutableListOf<Category>()
+        for (j in 0 until 5){
+            val movies = mutableListOf<Movie>()
+            for(i in 0 until 8){
+                val movie = Movie(R.drawable.img_1)
+                movies.add(movie)
+            }
+            val category = Category("cat $j", movies)
+            categories.add(category)
         }
 
-        val rv: RecyclerView = findViewById(R.id.recycler_view)
-        val adapter = MainAdapter(movies)
-        rv.layoutManager = LinearLayoutManager(this, RecyclerView.HORIZONTAL, false)
+        val adapter = CategoryAdapter(categories)
+        val rv: RecyclerView = findViewById(R.id.rv_main)
+        rv.layoutManager = LinearLayoutManager(this)
         rv.adapter = adapter
     }
 
--- app/src/main/java/com/example/filmex/MovieAdapter.kt
+++ app/src/main/java/com/example/filmex/MovieAdapter.kt
@@ -1,16 +1,13 @@
-package com.example.filmex.adapter
+package com.example.filmex
 
 import android.view.LayoutInflater
-import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.filmex.R
 import com.example.filmex.model.Movie
 
-class MainAdapter(private val movies:List<Movie>) : RecyclerView.Adapter<MainAdapter.MovieViewHolder>(){
+class MovieAdapter(private val movies:List<Movie>) : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>(){
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
