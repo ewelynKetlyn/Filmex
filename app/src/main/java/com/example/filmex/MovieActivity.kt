06b1bf9cd1eec75611499391a5a60895e61e08a5
Ewelyn <ewelyn.silva@picpay.com>
2022-07-05 09:41:11 -0300

    Adição do sombreamento da imageView

--- app/src/main/java/com/example/filmex/MovieActivity.kt
+++ app/src/main/java/com/example/filmex/MovieActivity.kt
@@ -1,8 +1,12 @@
 package com.example.filmex
 
+import android.content.Context
+import android.graphics.drawable.LayerDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
 
 class MovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +18,11 @@ class MovieActivity : AppCompatActivity() {
 
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val layerDrawable : LayerDrawable = ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
+        val movieCover = ContextCompat.getDrawable(this, R.drawable.img_1)
+        layerDrawable.setDrawableByLayerId(R.id.cover_drawble, movieCover)
+        val coverImg: ImageView = findViewById(R.id.movie_img)
+        coverImg.setImageDrawable(layerDrawable)
     }
 }
